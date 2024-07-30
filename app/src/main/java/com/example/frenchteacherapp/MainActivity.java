aa6c3fa317cedce342391a5f03cf4a17e4983712
HAMZA <hamzajaa2017@gmail.com>
2024-07-30 20:22:12 +0100

    add...

--- app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.frenchteacherapp;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,13 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.IllegalFormatCodePointException;
 
-    Button buttonBlack;
-    Button buttonGreen;
-    Button buttonPurple;
-    Button buttonRed;
-    Button buttonYellow;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonBlack, buttonGreen, buttonPurple, buttonRed, buttonYellow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +34,59 @@ public class MainActivity extends AppCompatActivity {
         buttonRed = findViewById(R.id.buttonRed);
         buttonYellow = findViewById(R.id.buttonYellow);
 
-        buttonBlack.setOnClickListener(v -> {
-            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.black);
-            mediaPlayer.start();
-        });
+        buttonBlack.setOnClickListener(this);
+        buttonGreen.setOnClickListener(this);
+        buttonPurple.setOnClickListener(this);
+        buttonRed.setOnClickListener(this);
+        buttonYellow.setOnClickListener(this);
 
-        buttonGreen.setOnClickListener(v -> {
-            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.green);
-            mediaPlayer.start();
-        });
+//        buttonBlack.setOnClickListener(v -> {
+//            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.black);
+//            mediaPlayer.start();
+//        });
+//
+//        buttonGreen.setOnClickListener(v -> {
+//            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.green);
+//            mediaPlayer.start();
+//        });
+//
+//        buttonPurple.setOnClickListener(v -> {
+//            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.purple);
+//            mediaPlayer.start();
+//        });
+//
+//        buttonRed.setOnClickListener(v -> {
+//            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.red);
+//            mediaPlayer.start();
+//        });
+//
+//        buttonYellow.setOnClickListener(v -> {
+//            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.yellow);
+//            mediaPlayer.start();
+//        });
 
-        buttonPurple.setOnClickListener(v -> {
-            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.purple);
-            mediaPlayer.start();
-        });
+    }
 
-        buttonRed.setOnClickListener(v -> {
-            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.red);
-            mediaPlayer.start();
-        });
 
-        buttonYellow.setOnClickListener(v -> {
-            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.yellow);
-            mediaPlayer.start();
-        });
+    public void playSound(int sound) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, sound);
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int clickedId = v.getId();
+        if (clickedId == R.id.buttonBlack) {
+            playSound(R.raw.black);
+        } else if (clickedId == R.id.buttonGreen) {
+            playSound(R.raw.green);
+        } else if (clickedId == R.id.buttonPurple) {
+            playSound(R.raw.purple);
+        } else if (clickedId == R.id.buttonRed) {
+            playSound(R.raw.red);
+        } else if (clickedId == R.id.buttonYellow) {
+            playSound(R.raw.yellow);
+        }
 
     }
 }
